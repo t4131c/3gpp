e0d4b50091099330e09ba5971429a590809a6046
t4131c <48999248+t4131c@users.noreply.github.com>
2022-12-01 00:27:09 +0900

    Add files via upload

---
+++ ljh.py
@@ -0,0 +1,78 @@
+import re
+from urllib import request
+import wget
+import os
+import docx2txt 
+import zipfile
+import subprocess
+homepath = ""
+ 
+
+def downloadzip(url, name):
+  path = homepath + "/zips/" + name + "/"
+  docpath = homepath + "/Docs/" + name + "/"
+  print(path)
+  if (not os.path.isdir(path)):
+    os.mkdir(path)
+  if (not os.path.isdir(docpath)):
+    os.mkdir(docpath)
+
+  html=request.urlopen(url) 
+  html_contents=str(html.read().decode("cp949"))
+  series_list = re.findall(r"(https)(.+)(.zip\">)", html_contents) 
+  for url in series_list: 
+    tmp_url="".join(url) 
+    final_url = tmp_url[:tmp_url.find('"')]
+    zipname = wget.download(final_url, out=path)
+    unzip(zipname, name);
+
+
+def unzip(filename, name):
+
+  path = homepath + "/Docs/" + name + "/"
+  with zipfile.ZipFile(filename, 'r') as zipObj:
+   listOfFileNames = zipObj.namelist()
+   for tmpfile in listOfFileNames:
+      if tmpfile.endswith("doc") or tmpfile.endswith("docx"):
+         zipObj.extract(tmpfile, path=path)
+         subprocess.call(['soffice', '--headless', '--convert-to', 'txt:Text', path + tmpfile,'-outdir', homepath + "/txts/" + name + "/"])
+      if tmpfile.endswith("zip"):
+        zipObj.extract(tmpfile)
+        with zipfile.ZipFile(tmpfile, 'r') as test:
+          tmplist = test.namelist()
+          for tmp in tmplist:
+            if tmp.endswith("doc") or tmp.endswith("docx"):
+              test.extract(tmp, path=path)
+              subprocess.call(['soffice', '--headless', '--convert-to', 'txt:Text', path + tmp,'-outdir', homepath + "/txts/" + name + "/"]) 
+
+   
+
+
+
+if __name__ == '__main__':
+  homepath = os.getcwd()
+  if (not os.path.isdir(homepath + "/Docs")):
+    os.mkdir(homepath + "/Docs")
+  if (not os.path.isdir(homepath + "/txts")):
+    os.mkdir(homepath + "/txts")
+  if (not os.path.isdir(homepath + "/zips")):
+    os.mkdir(homepath + "/zips")
+  url="https://www.3gpp.org/ftp/tsg_sa/WG3_Security/"
+  html=request.urlopen(url) 
+  html_contents=str(html.read().decode("cp949"))
+  series_list = re.findall(r"(https)(.+)(>TSGS3)", html_contents) 
+
+  for url in series_list: 
+    tmp_url="".join(url) 
+    final_url = tmp_url[:tmp_url.find('"')] + "/Docs"
+
+    try:
+      version = int(re.findall(r'\d+',final_url[final_url.find("TSGS3_") + len("TSGS3_"):])[0])
+      if not ((88 <= version and version <= 91)):
+        continue
+    except:
+      break
+    print("Downloading : " + final_url)
+    
+    name = final_url[final_url.find("TSGS3_"):final_url.find("/Docs")]
+    downloadzip(final_url, name)
